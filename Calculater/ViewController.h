53c15fe937597239e6355ee7cd3e68679c5b542e
YOUR NAME <YOUR@EMAIL.com>
2015-04-17 10:33:04 +0530

    Initial Commit

---
+++ Calculater/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculater
+//
+//  Created by Vijay Pratap Singh on 17/04/15.
+//  Copyright (c) 2015 Vijay Pratap Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculater/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calculater
+//
+//  Created by Vijay Pratap Singh on 17/04/15.
+//  Copyright (c) 2015 Vijay Pratap Singh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
